22cf42f46c822807e7307669ec908795024b9fef
Vijay-kumar10 <kumar888985@gmail.com>
2025-08-02 12:18:54 +0530

    Create Parking_Lot project to implement LLD.

---
+++ src/test/java/com/parkinglotlld/ParkingLotLldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkinglotlld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotLldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
